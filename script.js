989efade9094b56e1fc192a987ad6f37c22d105b
jorob098 <agatyocla@gmail.com>
2025-07-07 21:41:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
   const toggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('nav ul');
+  const menu = document.querySelector('.mobile-menu ul');
 
   toggle.addEventListener('click', function() {
     menu.classList.toggle('show');
