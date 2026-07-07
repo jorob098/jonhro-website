3bc5969723c01204eb5d8f7b5aea3562273172ee
jorob098 <agatyocla@gmail.com>
2026-07-07 16:38:58 +0800

    Update main.jsx
    style.css to Style.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { HashRouter } from 'react-router-dom'; // ✅ Use HashRouter instead
-import './style.css';
+import './Style.css';
 import "./i18n"; // Load translations
 
 ReactDOM.createRoot(document.getElementById('root')).render(
